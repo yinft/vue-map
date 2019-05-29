b244881330941badda1b5b0a5e7dc875fc65ac73
yinft <yinft@zencloud.net.cn>
2019-05-29 22:36:02 +0800

    vue结合高德地图加载特定位置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,113 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+<div class="amap-page-container">
+      <el-amap vid="amapDemo" :zoom="zoom" :center="center" class="amap-demo">
+        
+        <el-amap-marker  :position="marker1.position"  :events="marker1.events" :visible="marker1.visible" :draggable="marker1.draggable" :vid='marker1.vid' ></el-amap-marker>
+      </el-amap>
+</div>
+
   </div>
+
+
+
+
+
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      count: 1,
+      slotStyle: {
+        padding: "2px 8px",
+        background: "#eee",
+        color: "#333",
+        border: "1px solid #aaa"
+      },
+      zoom: 17,
+      center: [119.330532, 32.146321],
+      marker1: {
+        vid: "1",
+
+        icon:
+          "https://img-1255851908.cos.ap-shanghai.myqcloud.com/lALPDgQ9quAXipM3Nw_55_55.png_620x10000q90g.png",
+        position: [119.33252, 32.14781],
+        events: {
+          click: () => {
+            alert("click marker1");
+          }
+        },
+
+        draggable: false,
+        template: "<span>1</span>"
+      },
+      marker2: {
+        position: [119.33392, 32.14801],
+        events: {
+          click: () => {
+            alert("click marker2");
+          }
+        },
+        visible: true,
+        draggable: false,
+        template: "<span>1</span>"
+      },
+      marker3: {
+        position: [119.32812, 32.14571],
+        events: {
+          click: () => {
+            alert("click marker3");
+          }
+        },
+        visible: true,
+        draggable: false,
+        template: "<span>1</span>"
+      },
+      marker4: {
+        position: [119.33152, 32.14581],
+        events: {
+          click: () => {
+            alert("click marker4");
+          }
+        },
+        visible: true,
+        draggable: false,
+        template: "<span>1</span>"
+      },
+      marker5: {
+        position: [119.33252, 32.14601],
+        events: {
+          click: () => {
+            alert("click marker5");
+          }
+        },
+        visible: true,
+        draggable: false,
+        template: "<span>1</span>"
+      },
+      marker6: {
+        position: [119.33352, 32.14611],
+        events: {
+          click: () => {
+            alert("click marker6");
+          }
+        },
+        visible: true,
+        draggable: false,
+        template: "<span>1</span>"
+      }
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +125,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.amap-demo {
+  height: 500px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAMap from 'vue-amap';
 
 Vue.config.productionTip = false
+
+
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: 'dd708576aac75e02e2ad604296d60b6b',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+});
 
 new Vue({
   render: h => h(App),
